de76b840963174c34662037588a83456f45d4121
Ahmed Alwadyani <20197363@stu.uob.edu.bh>
2024-12-02 21:31:05 +0300

    edit

--- connection.php
+++ connection.php
@@ -4,7 +4,7 @@ $dsn = 'mysql:host=localhost;dbname=roombooking';
 $username = 'root';
 $password = '';
 
-$db = new PDO($dsn,$username,$password);
+$conn = new PDO($dsn,$username,$password);
 
 
 ?>
